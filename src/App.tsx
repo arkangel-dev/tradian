25cbc806919ae5fe47c43c2c33181aaa1b394ef5
arkangel-dev <noreply@arkangel.live>
2024-12-04 17:29:22 +0500

    Code upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-blue-500 text-red-200 p-4">
+    <h1 className="text-3xl font-bold underline">Hello, Tailwind CSS!</h1>
+  </div>
+  )
+}
+
+export default App
